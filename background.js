2aad252e5549158a2b8943ca5a5a8d9ddcbe1e6e
mggower <gowermikey@gmail.com>
2020-10-29 14:04:46 -0400

    completion

--- background.js
+++ background.js
@@ -3,6 +3,6 @@ chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(tab) {
   if (push === 5) push = 0;
-  push += 1;
-  chrome.tabs.sendMessage(tab.id, push);
+  let message = 'Ready!';
+  chrome.tabs.sendMessage(tab.id, message);
 }
--- content.js
+++ content.js
@@ -1,71 +1,58 @@
 chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(push, sender, sendResponse) {
-  console.log(push);
-
-  // const newImg = document.createElement('img');
-  // body.appendChild(newImg);
-  // newImg.style.maxWidth = '700px';
-  // newImg.style.padding = '50px';
-  // newImg.src = `${images[push - 1]}`;
-
-  // console.log(chrome.history.getVisits('www.facebook.com', () => 'hey'));
-  // const img = document.querySelectorAll('img');
-  // const btns = document.querySelectorAll('button');
-  // const h1 = document.querySelectorAll('h1');
-  // const a = document.querySelectorAll('a');
-  // const span = document.querySelectorAll('span');
-  // const posts = document.querySelectorAll('div._9AhH0');
-
-  // console.log(posts);
-
-  // btns.forEach((x) => (x.innerText = '0'));
-  // span.forEach((x) => (x.innerText = 'there is nothing to see here'));
-  // h1.forEach((x) => (x.innerText = 'you can do it!'));
-  // a.forEach((x) => (x.innerText = 'it doesnt matter'));
-  // posts.forEach((x) => x.innerHTML = `${}`)
-  // img.forEach((x) => (x.src = `${images[push - 1]}`));
+function gotMessage(message, sender, sendResponse) {
+  console.log(message);
 }
-
 const images = [
+  'https://i.ibb.co/9yj56GV/dog1.jpg',
+  'https://i.ibb.co/NsxVVbz/dog2.jpg',
+  'https://i.ibb.co/RQmNyhj/Screen-Shot-2020-10-29-at-13-55-09.png',
+  'https://i.ibb.co/NnrZjhY/Image-from-i-OS.jpg',
+  'https://i.ibb.co/rtLcyKs/Image-from-i-OS-3.jpg',
+  'https://i.ibb.co/gtnfhKH/Image-from-i-OS-2.jpg',
+  'https://i.ibb.co/pwKYRdH/Image-from-i-OS-1.jpg',
+  'https://i.ibb.co/bJBGBWK/image.png',
   'https://www.thesprucepets.com/thmb/ptFV8TOmUN_emxDYZRRXcMykEpE=/3168x1782/smart/filters:no_upscale()/puppy-samoyed-boy-990077480-5c89719646e0fb00012c67e8.jpg',
   'https://www.dogtime.com/assets/uploads/2018/10/puppies-cover-1280x720.jpg',
   'https://global-uploads.webflow.com/5eb69d64ca6f425ad59d4319/5ec6ae23d5782441f4252723_littermate%20syndrome%20two%20puppies.png',
   'https://www.dogtime.com/assets/uploads/2016/09/beagle-puppy-5-e1568913038666.jpg',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWApY7vpCoiyrYKL1FUsfNDwYUSNPTG5TZlQ&usqp=CAU',
 ];
+const imgLen = images.length - 1;
+
+const headlines = [
+  'Heads Up for Tails!',
+  'OMG SO FLUFFY',
+  'Puppy Leaks!',
+  'Oh My Dog!',
+  "It's Dog or Nothing!",
+  'Green puppy born in Italy',
+  'Puppies in high demand!',
+  'Three cute new puppies!',
+];
+
+const hLen = headlines.length - 1;
+
 const body = document.querySelector('body');
 const img = document.querySelectorAll('img');
-// body.style.width = '100vw';
-// body.style.height = 'auto';
-// body.style.display = 'flex';
-// body.style.flexFlow = 'column';
-// body.style.justifyContent = 'center';
-// body.style.alignItems = 'center';
+const span = document.querySelectorAll('span');
+const h1 = document.querySelectorAll('h1');
 
-// console.log(new Date.now());
 img.forEach((x) =>
   x.addEventListener('click', (e) => {
     e.preventDefault();
-    return x.src = `${images[Math.floor(Math.random() * 4)]}`;
+    return (x.src = `${images.shift()}`);
+  })
+);
+span.forEach((x) =>
+  x.addEventListener('click', (e) => {
+    e.preventDefault();
+    return (x.innerText = `${headlines[Math.floor(Math.random() * hLen)]}`);
   })
 );
 
-// x.src = `${images[push - 1]}`;
-
-// const re = new RegExp('chrome', 'gi');
-// const matches = document.documentElement.innerHTML.match(re) || [];
-//
-// chrome.runtime.sendMessage({
-//   url: window.location.href,
-//   count: matches.length,
-// });
-
-/*
-// method 1: regular expression literal notation — enclosed between slashes
-str.replace(/hello/, 'hi');
-
-// method 2: calling the constructor function of the RegExp object
-str.replace(new RegExp('hello'), 'hi');
-
-// Output: hi world! hello people! Hello hello!
-*/
+h1.forEach((x) =>
+  x.addEventListener('click', (e) => {
+    e.preventDefault();
+    return (x.innerText = `${headlines[Math.floor(Math.random() * hLen)]}`);
+  })
+);
